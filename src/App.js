4aedd55403b5fe518c59ac88178030a75962a6c0
freddydumont <frederickmorin18@gmail.com>
2017-07-16 14:24:11 -0400

    Add title image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import title from './images/title.png'
 import './styles/app.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <img src={title} alt="Poké(Si)mon" />
       </div>
     );
   }
