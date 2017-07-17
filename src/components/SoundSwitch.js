9656444f72679b0041ee965e2dbb88f1d58bb014
freddydumont <frederickmorin18@gmail.com>
2017-07-17 15:29:05 -0400

    Implement switching game sounds

--- src/components/SoundSwitch.js
+++ src/components/SoundSwitch.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { switchSounds } from '../actions';
 import soundsPkm from '../images/sounds_pkm.svg';
 import soundsSmn from '../images/sounds_smn.svg';
 
-const SoundSwitch = () => {
+let SoundSwitch = ({ isPokeSounds, switchSounds }) => {
   return (
-    <img className="switch" src={soundsPkm} alt="Sound Switch" />
+    <img
+      className="switch"
+      src={isPokeSounds ? soundsPkm : soundsSmn}
+      onClick={switchSounds}
+      alt="Sound Switch" />
   );
 }
+
+const mapStateToProps = state => {
+  return {
+    isPokeSounds: state.isPokeSounds
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    switchSounds: () => dispatch(switchSounds())
+  }
+}
+
+SoundSwitch = connect(mapStateToProps, mapDispatchToProps)(SoundSwitch);
 
 export default SoundSwitch;
