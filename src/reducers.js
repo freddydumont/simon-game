eaa4e3bcefa47d5729e50c700d24bbf30132ef5d
freddydumont <frederickmorin18@gmail.com>
2017-07-17 17:55:50 -0400

    Add initial sounds arrays to store

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,33 @@
 import { combineReducers } from 'redux';
+import { Howl } from 'howler';
 import { SWITCH_MODE, SWITCH_SOUNDS } from './actions.js';
+// import sounds
+import bulbasaur from './sounds/cry-bulbasaur.mp3'
+import charmander from './sounds/cry-charmander.mp3'
+import pikachu from './sounds/cry-pikachu.mp3'
+import squirtle from './sounds/cry-squirtle.mp3'
+import simonSound1 from './sounds/simonSound1.mp3'
+import simonSound2 from './sounds/simonSound2.mp3'
+import simonSound3 from './sounds/simonSound3.mp3'
+import simonSound4 from './sounds/simonSound4.mp3'
+
+// create initial pokeSounds array
+let pokeSoundsArray = [bulbasaur, charmander, pikachu, squirtle];
+let initialPokeSounds = [];
+for (let i = 0; i < 4; i++) {
+  initialPokeSounds.push(new Howl({
+    src: [pokeSoundsArray[i]]
+  }))
+}
+
+// create initial simonSounds array
+let simonSoundsArray = [simonSound4, simonSound3, simonSound2, simonSound1]
+let initialSimonSounds = [];
+for (let i = 0; i < 4; i++) {
+  initialSimonSounds.push(new Howl({
+    src: [simonSoundsArray[i]]
+  }))
+}
 
 // STRICT MODE
 function isStrictMode(state = false, action) {
@@ -21,5 +49,19 @@ function isPokeSounds(state = true, action) {
   }
 };
 
-const reducer = combineReducers({ isStrictMode, isPokeSounds });
+function pokeSounds(state = initialPokeSounds, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+function simonSounds(state = initialSimonSounds, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+const reducer = combineReducers({ isStrictMode, isPokeSounds, pokeSounds, simonSounds });
 export default reducer;
