cef227f4004822f1c2dbda65c30473d050a27032
freddydumont <frederickmorin18@gmail.com>
2017-07-17 15:14:31 -0400

    Add isPokeSounds reducer

--- src/reducers.js
+++ src/reducers.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { SWITCH_MODE } from './actions.js';
+import { SWITCH_MODE, SWITCH_SOUNDS } from './actions.js';
 
 // STRICT MODE
 function isStrictMode(state = false, action) {
@@ -11,5 +11,15 @@ function isStrictMode(state = false, action) {
   }
 };
 
-const reducer = combineReducers({ isStrictMode });
+// SOUNDS
+function isPokeSounds(state = true, action) {
+  switch (action.type) {
+    case SWITCH_SOUNDS:
+      return !state;
+    default:
+      return state;
+  }
+};
+
+const reducer = combineReducers({ isStrictMode, isPokeSounds });
 export default reducer;
