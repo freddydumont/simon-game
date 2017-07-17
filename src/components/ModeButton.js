663bf974f3ad36b2a2eb287feb80855e540c1600
freddydumont <frederickmorin18@gmail.com>
2017-07-17 14:52:36 -0400

    Implement switching between regular and strict mode

--- src/components/ModeButton.js
+++ src/components/ModeButton.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { switchMode } from '../actions';
 import regular from '../images/regular.svg';
 import strict from '../images/strict.svg';
 
-const ModeButton = () => {
+let ModeButton = ({ isStrictMode, switchMode }) => {
   return (
-    <img className="btn-mode" src={regular} alt="Mode button" />
+    <img
+      className="btn-mode"
+      src={isStrictMode ? strict : regular}
+      onClick={switchMode}
+      alt="Mode button" />
   );
 }
+
+const mapStateToProps = state => {
+  return {
+    isStrictMode: state.isStrictMode
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    switchMode: () => dispatch(switchMode())
+  }
+}
+
+ModeButton = connect(mapStateToProps, mapDispatchToProps)(ModeButton);
 
 export default ModeButton;
