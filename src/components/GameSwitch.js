eb437eb6de6f9e4fc558dc9fa8568667ddf50802
freddydumont <frederickmorin18@gmail.com>
2017-07-17 21:32:28 -0400

    Implement switching game on and off

--- src/components/GameSwitch.js
+++ src/components/GameSwitch.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { switchGame } from '../actions';
 import gameOn from '../images/game_on.svg';
 import gameOff from '../images/game_off.svg';
 
-const GameSwitch = () => {
+let GameSwitch = ({ isGameStarted, switchGame }) => {
   return (
-    <img className="switch" src={gameOn} alt="Game Switch" />
+    <img
+      className="switch"
+      src={isGameStarted ? gameOn : gameOff}
+      onClick={switchGame}
+      alt="Game Switch" />
   );
 }
 
+const mapStateToProps = state => {
+  return {
+    isGameStarted: state.isGameStarted
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    switchGame: () => dispatch(switchGame())
+  }
+}
+
+GameSwitch = connect(mapStateToProps, mapDispatchToProps)(GameSwitch);
 export default GameSwitch;
