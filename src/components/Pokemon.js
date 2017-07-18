f0171b93d053db13a281c14338b11cbf4e10c5e1
freddydumont <frederickmorin18@gmail.com>
2017-07-18 16:58:00 -0400

    Refactor Pokemon to receive image as prop

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import bulbasaur from '../images/bulbasaur.svg';
-import charmander from '../images/charmander.svg';
-import pikachu from '../images/pikachu.svg';
-import squirtle from '../images/squirtle.svg';
 
-const Pokemon = ({ pos }) => {
-  // assign pokemon image according to position in grid
-  let pokemon;
-  switch (pos) {
-    case 1: pokemon = bulbasaur;
-      break;
-    case 2: pokemon = charmander;
-      break;
-    case 3: pokemon = pikachu;
-      break;
-    case 4: pokemon = squirtle;
-      break;
-    default: break;
-  }
-
+const Pokemon = ({ pos, image }) => {
   return (
     <div className={`pokemon item-${pos}`} >
-      <img src={pokemon} alt="Pokemon" />
+      <img src={image} alt="Pokemon" />
     </div>
   );
 }
