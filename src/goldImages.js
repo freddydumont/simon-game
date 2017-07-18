a5816e8ee823ea6dc8ff6f7d4d9699bb8bad4511
freddydumont <frederickmorin18@gmail.com>
2017-07-18 11:19:20 -0400

    Add goldImages file
    Import gold images and export them as an object
    Add gold0

---
+++ src/goldImages.js
@@ -0,0 +1,46 @@
+import gold0 from './images/gold/gold_00.svg';
+import gold1 from './images/gold/gold_01.svg';
+import gold2 from './images/gold/gold_02.svg';
+import gold3 from './images/gold/gold_03.svg';
+import gold4 from './images/gold/gold_04.svg';
+import gold5 from './images/gold/gold_05.svg';
+import gold6 from './images/gold/gold_06.svg';
+import gold7 from './images/gold/gold_07.svg';
+import gold8 from './images/gold/gold_08.svg';
+import gold9 from './images/gold/gold_09.svg';
+import gold10 from './images/gold/gold_10.svg';
+import gold11 from './images/gold/gold_11.svg';
+import gold12 from './images/gold/gold_12.svg';
+import gold13 from './images/gold/gold_13.svg';
+import gold14 from './images/gold/gold_14.svg';
+import gold15 from './images/gold/gold_15.svg';
+import gold16 from './images/gold/gold_16.svg';
+import gold17 from './images/gold/gold_17.svg';
+import gold18 from './images/gold/gold_18.svg';
+import gold19 from './images/gold/gold_19.svg';
+import gold20 from './images/gold/gold_20.svg';
+
+// exporting as an object to be able to access keys dynamically in a separate file
+export default {
+  gold0,
+  gold1,
+  gold2,
+  gold3,
+  gold4,
+  gold5,
+  gold6,
+  gold7,
+  gold8,
+  gold9,
+  gold10,
+  gold11,
+  gold12,
+  gold13,
+  gold14,
+  gold15,
+  gold16,
+  gold17,
+  gold18,
+  gold19,
+  gold20
+}
