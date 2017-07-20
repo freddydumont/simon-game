5bfc7c500dd562c6354357536c540b2a9e1033d6
freddydumont <frederickmorin18@gmail.com>
2017-07-20 14:39:34 -0400

    Revert "Add sequence-action middleware"
    This reverts commit 00724c4fd3d64ff6c0a3fe09fe4589ed6f13b2a2.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, compose } from 'redux';
 import { Provider } from 'react-redux';
-import sequenceAction from 'redux-sequence-action';
 import App from './containers/App';
 import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(reducer, composeEnhancers(applyMiddleware(sequenceAction)));
+let store = createStore(reducer, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store} >
