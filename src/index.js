00724c4fd3d64ff6c0a3fe09fe4589ed6f13b2a2
freddydumont <frederickmorin18@gmail.com>
2017-07-20 14:39:34 -0400

    Add sequence-action middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import sequenceAction from 'redux-sequence-action';
 import App from './containers/App';
 import reducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-let store = createStore(reducer, composeEnhancers());
+let store = createStore(reducer, composeEnhancers(applyMiddleware(sequenceAction)));
 
 ReactDOM.render(
   <Provider store={store} >
