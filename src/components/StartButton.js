d061576537af8fa6b61ebe048676dbd32c25c318
freddydumont <frederickmorin18@gmail.com>
2017-07-19 21:45:14 -0400

    Change image when StartButton is pressed

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import start from '../images/start.svg';
 import startPressed from '../images/start_pressed.svg';
 
-const StartButton = () => (
-  <img className="switch" src={start} alt="Start Button" />
-);
+class StartButton extends Component {
+  state = { image: start }
+  render() {
+    return (
+      <img
+        className="switch"
+        src={this.state.image}
+        onMouseDown={() => this.setState({ image: startPressed })}
+        onMouseUp={() => this.setState({ image: start })}
+        alt="Start Button" />
+    );
+  }
+}
 
+const mapStateToProps = state => {
+  return {
+    isGameStarted: state.isGameStarted
+  }
+}
+
+StartButton = connect(mapStateToProps)(StartButton);
 export default StartButton;
