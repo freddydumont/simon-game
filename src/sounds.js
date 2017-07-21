b6aeb940c3dcfffb4261792507b2019acd9efe74
freddydumont <frederickmorin18@gmail.com>
2017-07-21 19:13:26 -0400

    Set simon sounds rate to half

--- src/sounds.js
+++ src/sounds.js
@@ -8,19 +8,20 @@ import simonSound2 from './sounds/simonSound2.mp3'
 import simonSound3 from './sounds/simonSound3.mp3'
 import simonSound4 from './sounds/simonSound4.mp3'
 
+let pokeSounds = [bulbasaur, charmander, pikachu, squirtle];
+let simonSounds = [simonSound4, simonSound3, simonSound2, simonSound1];
+
 // take an array of mp3s and turn it into an array of howls
 function createSounds(soundSource) {
   let sounds = [];
   for (let i = 0; i < 4; i++) {
     sounds.push(new Howl({
-      src: [soundSource[i]]
+      src: [soundSource[i]],
+      rate: soundSource === simonSounds ? 0.5 : 1
     }))
   }
   return sounds;
 }
 
 // export an array containing an array of poke sounds and one of simon sounds
-export default [
-  createSounds([bulbasaur, charmander, pikachu, squirtle]),
-  createSounds([simonSound4, simonSound3, simonSound2, simonSound1])
-]
+export default [createSounds(pokeSounds), createSounds(simonSounds)]
