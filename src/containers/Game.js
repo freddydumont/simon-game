d194420cc497aeba76355c49286c86641d9d77a4
freddydumont <frederickmorin18@gmail.com>
2017-07-21 11:28:15 -0400

    Add glowing as a prop to Game

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -3,16 +3,16 @@ import { connect } from 'react-redux';
 import Pokemon from '../components/Pokemon';
 import pokeImages from '../pokemonImages';
 
-let Game = () => {
+let Game = ({ glowing }) => {
   return (
     <div className="game">
       <div className="rectangle"></div>
       <div className="circle-black"></div>
       <div className="circle-white pokemon-container">
-        <Pokemon image={pokeImages['bulbasaur']} />
-        <Pokemon image={pokeImages['charmander']} />
-        <Pokemon image={pokeImages['pikachu']} />
-        <Pokemon image={pokeImages['squirtle']} />
+        <Pokemon image={pokeImages[glowing === 0 ? 'bulbasaurGlow' : 'bulbasaur']} />
+        <Pokemon image={pokeImages[glowing === 1 ? 'charmanderGlow' : 'charmander']} />
+        <Pokemon image={pokeImages[glowing === 2 ? 'pikachuGlow' : 'pikachu']} />
+        <Pokemon image={pokeImages[glowing === 3 ? 'squirtleGlow' : 'squirtle']} />
       </div>
     </div>
   );
@@ -23,7 +23,8 @@ const mapStateToProps = state => {
     isGameStarted: state.isGameStarted,
     isStrictMode: state.isStrictMode,
     sounds: state.isPokeSounds ? state.pokeSounds : state.simonSounds,
-    sequence: state.sequence ? state.sequence.slice(0, state.count) : null
+    sequence: state.sequence ? state.sequence.slice(0, state.count) : null,
+    glowing: state.glowing
   }
 }
 
