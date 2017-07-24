1009a0f506c5bc9380f1ce48f2c4a0180d79ee2e
freddydumont <frederickmorin18@gmail.com>
2017-07-24 14:33:55 -0400

    Send poke number with clickPokemon

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { clickPokemon } from '../actions';
 
-let Pokemon = ({ image, isPlayerTurn, clickPokemon }) => {
+let Pokemon = ({ image, key, isPlayerTurn, clickPokemon }) => {
   return (
     <div className="pokemon" >
       <img
         src={image} alt="Pokemon"
-        onClick={isPlayerTurn ? clickPokemon : null} />
+        onClick={() => {
+          if (isPlayerTurn) {
+            clickPokemon(key);
+          }
+        }} />
     </div>
   );
 }
--- src/containers/Game.js
+++ src/containers/Game.js
@@ -9,10 +9,10 @@ let Game = ({ glowing }) => {
       <div className="rectangle"></div>
       <div className="circle-black"></div>
       <div className="circle-white pokemon-container">
-        <Pokemon image={pokeImages[glowing === 0 ? 'bulbasaurGlow' : 'bulbasaur']} />
-        <Pokemon image={pokeImages[glowing === 1 ? 'charmanderGlow' : 'charmander']} />
-        <Pokemon image={pokeImages[glowing === 2 ? 'pikachuGlow' : 'pikachu']} />
-        <Pokemon image={pokeImages[glowing === 3 ? 'squirtleGlow' : 'squirtle']} />
+        <Pokemon image={pokeImages[glowing === 0 ? 'bulbasaurGlow' : 'bulbasaur']} key={0} />
+        <Pokemon image={pokeImages[glowing === 1 ? 'charmanderGlow' : 'charmander']} key={1} />
+        <Pokemon image={pokeImages[glowing === 2 ? 'pikachuGlow' : 'pikachu']} key={2} />
+        <Pokemon image={pokeImages[glowing === 3 ? 'squirtleGlow' : 'squirtle']} key={3} />
       </div>
     </div>
   );
